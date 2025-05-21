c3f7bc5a63dba54988892640aae32376cc014acc
Ssengendo Naz Hill (POM) <nssegendo@projectcode.ug>
2025-05-21 13:56:56 +0300

    dis play client on the tab

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
 {{ Collection }}
  <catFlips v-memo @returnedData='returnedPaginatedData'  :outorelaod='loading_statuses'
           :data="{ Rows: 100, ...tileTofetch, both: 1, search_keyword }" StateName='collectionStateList'
-          :url="'http://127.0.0.1:8000/api/v3/collection'" />
+          :url="'http://127.0.0.1:8000/api/v3/patients'" />
 </template>
 <script setup>
 import { computed, ref } from 'vue'
 import { setBearerToken } from 'septor-store'
-setBearerToken({token:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YyL2F1dGgvMmZhIiwiaWF0IjoxNzQ3Njc0MTAzLCJleHAiOjE3NDc3MTczMDMsIm5iZiI6MTc0NzY3NDEwMywianRpIjoic1JMOGUxQVNjd05pSDRuRyIsInN1YiI6IjIyIiwicHJ2IjoiY2I0ZWU5OTdiYjIyNTEyMTg0M2NiMmU1M2I3NGM2M2FkM2RlN2I0YiJ9.Rvwft4MuvpZpgImfPunvBXH-x656eNYQYOAA98z4lZc'})
+setBearerToken({token:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL3YyL2F1dGgvMmZhIiwiaWF0IjoxNzQ3ODE3MzQ1LCJleHAiOjE3NDc4NjA1NDUsIm5iZiI6MTc0NzgxNzM0NSwianRpIjoiVzJxMVlTRWhFVWhUbVA1aCIsInN1YiI6IjIyIiwicHJ2IjoiY2I0ZWU5OTdiYjIyNTEyMTg0M2NiMmU1M2I3NGM2M2FkM2RlN2I0YiJ9.GZaI0-6rbRmNsy9NpLSu-x17NQwg1hYU__VxVbR7Wx0'})
 
 
 const loading_statuses = ref(true)
